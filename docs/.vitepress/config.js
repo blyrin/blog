1b52daa748364fbb6e2a33aae0473c542e49d4a4
FlapyPan <flapypan@gmail.com>
2025-02-07 20:06:12 +0800

    去除 google tag

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -96,15 +96,6 @@ export default defineConfig({
         rel: 'stylesheet',
       },
     ],
-    ['script', { async: '', src: 'https://www.googletagmanager.com/gtag/js?id=G-1ZYSWJMS6Y' }],
-    [
-      'script',
-      {},
-      `window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config', 'G-1ZYSWJMS6Y');`,
-    ],
   ],
   vite: {
     css: {
