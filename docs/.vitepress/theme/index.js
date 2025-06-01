05dd50bdadbd0f6f00d6e86fd33b5dd8416b207a
blyrin <flapypan@gmail.com>
2025-06-01 20:37:55 +0800

    添加粒子特效

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,4 +1,16 @@
 import DefaultTheme from 'vitepress/theme-without-fonts'
 import './custom.css'
-
 export default DefaultTheme
+
+import confetti from 'canvas-confetti'
+
+document.addEventListener('DOMContentLoaded', () => {
+  const scalar = 2
+  confetti({
+    particleCount: 100,
+    spread: 300,
+    origin: { y: 0.6 },
+    scalar,
+    shapes: [confetti.shapeFromText({ text: '🍥', scalar })],
+  })
+})
