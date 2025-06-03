7665d36101724585691c9d4decfc8701e6a78804
blyrin <flapypan@gmail.com>
2025-06-04 00:06:06 +0800

    优化粒子特效触发时机

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -5,14 +5,19 @@ export default DefaultTheme
 import confetti from 'canvas-confetti'
 
 if (!import.meta.env.SSR) {
-  document.addEventListener('DOMContentLoaded', () => {
-    const scalar = 2
-    confetti({
-      particleCount: 100,
-      spread: 300,
-      origin: { y: 0.6 },
-      scalar,
-      shapes: [confetti.shapeFromText({ text: '🍥', scalar })],
-    })
+  const scalar = 2
+  const confettiOptions = {
+    particleCount: 100,
+    spread: 300,
+    origin: { y: 0.6 },
+    scalar,
+    shapes: [confetti.shapeFromText({ text: '🍥', scalar })],
+  }
+  const showParticles = () => confetti(confettiOptions)
+  window.addEventListener('load', () => {
+    showParticles()
+    document
+      .querySelector('.VPHero.VPHomeHero.has-image .image-container img')
+      ?.addEventListener('click', showParticles)
   })
 }
